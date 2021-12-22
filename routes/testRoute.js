e8b90518b22ddee17a5482ccae71be03e7b74158
khem112 <ksmarathe.indian@gmail.com>
2021-12-22 21:59:07 +0530

    usa servers

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/test", (req, res) => {
+  const date = new Date();
   res.status(200).json({
+    date,
     msg: "hey successfully tested",
   });
 });
